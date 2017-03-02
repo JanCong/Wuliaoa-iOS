bb05ed3bd31c09600cd473200ee6a9416de442e2
bhk545145 <bhk545145@163.com>
2017-03-02 20:20:16 +0800

    111

--- ItcastWeibo/Classes/Compose(发微博)/Controller/IWComposeViewController.m
+++ ItcastWeibo/Classes/Compose(发微博)/Controller/IWComposeViewController.m
@@ -22,6 +22,11 @@
 @property (nonatomic, weak) UITextView *textView;
 @property (nonatomic, weak) IWComposeToolbar *toolbar;
 @property (weak, nonatomic) UIImageView *imageView;
+@property (nonatomic,strong) NSMutableArray *imageArray;//存放处理完的图片
+@property (nonatomic,strong) UIScrollView *scrolView;//滚动视图
+@property (nonatomic,strong) NSMutableArray *scrollSubViews;//存放图片子视图
+@property (nonatomic,strong) NSMutableArray *scrollSubFrame;//子视图的frame
+@property (nonatomic,strong) NSMutableArray *localLength;//每张图片的尺寸
 @end
 
 @implementation IWComposeViewController
@@ -29,7 +34,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
+    self.imageArray = [NSMutableArray new];
+    self.scrollSubViews = [NSMutableArray new];
+    self.scrollSubFrame = [NSMutableArray new];
+    self.localLength = [NSMutableArray new];
     [self setupNavBar];
     [self setupTextView];
     [self setupToolbar];
@@ -44,6 +52,12 @@
     imageView.clipsToBounds = YES;
     [self.textView addSubview:imageView];
     self.imageView = imageView;
+//    UIScrollView *scrol = [[UIScrollView alloc]init];
+//    scrol.frame = CGRectMake(0, 100*(SCREEN_HEIGHT/568)+ 64, 320*(SCREEN_WIDTH/320), 77*(SCREEN_HEIGHT/568));
+//    [self.textView addSubview:scrol];
+//    self.scrolView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
+//    self.scrolView = scrol;
+//    self.automaticallyAdjustsScrollViewInsets = NO;
 }
 
 - (void)setupToolbar
@@ -216,17 +230,152 @@
  */
 - (void)openAlbum
 {
-    UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
-    ipc.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
-    ipc.delegate = self;
-    [self presentViewController:ipc animated:YES completion:nil];
+    [self acquireLocal];
+//    UIImagePickerController *ipc = [[UIImagePickerController alloc] init];
+//    ipc.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+//    ipc.delegate = self;
+//    [self presentViewController:ipc animated:YES completion:nil];
 }
 
-- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
-{
-    [picker dismissViewControllerAnimated:YES completion:nil];
-    UIImage *image = info[UIImagePickerControllerOriginalImage];
-    self.imageView.image = image;
+#pragma mark --
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(nonnull NSDictionary<NSString *,id> *)info {
+    __block UIImage *originalImage = [info objectForKey:UIImagePickerControllerOriginalImage];
+    __weak IWComposeViewController *weakSelf = self;
+    void(^imageBlock)() = ^(UIImage *image){
+        __strong IWComposeViewController *strongSelf = weakSelf;
+        if (!image) {
+            image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        }
+        NSString *length  = [NSString stringWithFormat:@"%f*%f",image.size.width,image.size.height];
+        [strongSelf.localLength addObject:length];
+        [strongSelf.imageArray addObject:image];
+    };
+    void(^dismissBlock)() = ^(){//声明
+        __strong IWComposeViewController *strongSelf = weakSelf;
+        [picker dismissViewControllerAnimated:YES completion:^{
+            [strongSelf setSpread];
+        }];
+    };
+    //写入本地
+    [[LHPhotoList sharePhotoTool]saveImageToAblum:originalImage completion:^(BOOL success, PHAsset *asset) {
+        if (success) {//存成功
+            [[LHPhotoList sharePhotoTool]requestImageForAsset:asset size:CGSizeMake(1080, 1920) resizeMode:PHImageRequestOptionsResizeModeFast completion:^(UIImage *image, NSDictionary *info) {
+                originalImage = [UIImage imageWithData:UIImageJPEGRepresentation(originalImage, 1) scale:originalImage.scale];
+                imageBlock(originalImage);
+                dismissBlock();
+            }];
+        }else{//存取失败
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                originalImage = [UIImage imageWithData:UIImageJPEGRepresentation(originalImage, 1) scale:originalImage.scale];
+                imageBlock(originalImage);
+                dispatch_sync(dispatch_get_main_queue(), ^{
+                    dismissBlock();
+                });
+            });
+        }
+    }];
 }
+
+#pragma mark --
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark --
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark -获取本地图片
+-(void)acquireLocal{
+    LHGroupViewController *group = [[LHGroupViewController alloc]init];
+    group.maxChooseNumber = 80;
+    __weak IWComposeViewController *weakSelf = self;
+    group.backImageArray = ^(NSMutableArray<PHAsset *> *array){
+        __strong IWComposeViewController *strongSelf = weakSelf;
+        if (array) {
+            for (int i = 0; i<array.count; i++) {
+                PHAsset *asset = array[i];
+                [[LHPhotoList sharePhotoTool]requestImageForAsset:asset size:CGSizeMake(1080, 1920) resizeMode:PHImageRequestOptionsResizeModeFast completion:^(UIImage *image, NSDictionary *info) {
+                    NSString *length  = [NSString stringWithFormat:@"%f*%f",image.size.width,image.size.height];
+                    [_localLength addObject:length];
+                    [_imageArray addObject:image];
+                    [strongSelf setSpread];
+                }];
+            }
+        }
+    };
+    [self presentViewController:[[UINavigationController alloc] initWithRootViewController:group] animated:YES completion:nil];
+}
+
+#pragma mark -展示UI在界面
+-(void)setSpread{
+    self.imageView.image = self.imageArray[0];
+    self.scrolView.contentSize = CGSizeMake((imageWidth+10)*self.imageArray.count, 77*(SCREEN_HEIGHT/568));
+    for (NSInteger i = self.scrollSubViews.count; i<self.imageArray.count; i++) {
+        UIView *itemView = [[UIView alloc]init];
+        itemView.frame = CGRectMake(imageWidth*i+10*i, 5, imageWidth, imageWidth);
+        itemView.backgroundColor = [UIColor whiteColor];
+        [self.scrolView addSubview:itemView];
+        
+        UIImageView *imageView = [[UIImageView alloc]init];
+        imageView.frame = CGRectMake(0, 0, imageWidth, imageWidth);
+        imageView.image = self.imageArray[i];
+        imageView.userInteractionEnabled = YES;
+        imageView.contentMode = UIViewContentModeScaleAspectFill;
+        imageView.clipsToBounds = YES;
+        imageView.tag = 100 +i;
+        [itemView addSubview:imageView];
+        //手势
+        UIButton *deleteBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        deleteBtn.frame = CGRectMake(imageWidth - 17 , -5, 22, 22);
+        deleteBtn.tag = 200+i;
+        NSString *strDelete = [[NSBundle mainBundle]pathForResource:@"02" ofType:@"png"];
+        [deleteBtn setImage:[UIImage imageWithContentsOfFile:strDelete] forState:UIControlStateNormal];//正常显示
+        [deleteBtn addTarget:self action:@selector(deleteImage:) forControlEvents:UIControlEventTouchUpInside];//删除
+        [itemView addSubview:deleteBtn];
+        
+        [self.scrollSubFrame addObject:[NSValue valueWithCGRect:itemView.frame]];
+        [self.scrollSubViews addObject:itemView];
+        [UIView animateWithDuration:0.2 animations:^{
+            itemView.alpha = 1;
+        } completion:nil];
+    }
+    
+}
+
+#pragma mark -删除图片
+-(void)deleteImage:(UIButton *)btn{
+    UIView *view  = btn.superview;
+    NSInteger idx = [_scrollSubViews indexOfObject:view];
+    [_scrollSubViews removeObject:view];
+    [_imageArray removeObjectAtIndex:idx];
+    [_localLength removeObjectAtIndex:idx];
+    [_scrollSubFrame removeLastObject];
+    [UIView animateWithDuration:0.2 animations:^{
+        view.alpha = 0;
+    } completion:^(BOOL finished) {
+        [view removeFromSuperview];
+        self.scrolView.contentSize = CGSizeMake(imageWidth*self.scrollSubViews.count, imageWidth);
+        [UIView animateWithDuration:0.2 animations:^{
+            for (NSInteger i = idx; i < self.scrollSubViews.count; i++) {
+                UIView *item = self.scrollSubViews[i];
+                item.frame = [(NSValue*)(self.scrollSubFrame[i]) CGRectValue];
+            }
+        } completion:^(BOOL finished) {
+            if (finished) {
+                //完成之后处理的code
+            }
+        }];
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
 
 @end
